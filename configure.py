83650dd4fc9038d3573bebc407c2bec369b8c8bb
n2kkrd <65614635+n2kkrd@users.noreply.github.com>
2022-11-21 19:20:39 +0300

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,6 +1,6 @@
 config = {
-	'name': '568595708',
-	'token': '5914674048:AAHyUfJd0WlssrhPN8SU00WyIqhQTDvfDbc',
-	'tokenqiwi': '',
-	'phoneqiwi': ''
+	'name': 'Simki_genaBot',
+	'token': '5408085670:AAFHPAA3kpiwZWfXjWWItLM22IbeU4OT4Sc',
+	'tokenqiwi': '5e18f66df4e810398e4ce7f061625d67',
+	'phoneqiwi': '+79385116356'
 }
